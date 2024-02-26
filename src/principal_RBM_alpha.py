774007a1270736dad0293ad783bda1d7ee13255d
louislat <lmm.latournerie@gmail.com>
2024-02-26 13:02:54 +0100

    Reorganisation of scripts and notebooks

--- src/principal_RBM_alpha.py
+++ src/principal_RBM_alpha.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 
+def lire_alpha_digits(data, list_index):
+    m, n = data.shape
+    p, q = data[0, 0].shape
+    matrix = np.zeros((len(list_index)*n, p*q))
+    for i, index in enumerate(list_index):
+        for j in range(n):
+            matrix[i*n + j] = data[index, j].reshape(p*q)
+    return matrix
+
+
 class RbmModel():
     def __init__(self, q: int, p: int) -> None:
         """
